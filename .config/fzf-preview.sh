cd552055f8c7580cfcf61a31487f1cfeaa9b61d4
Grant Zhao <gzhao743@gmail.com>
2024-10-22 00:00:11 -0400

    switched to usr/bin/env

--- .config/fzf-preview.sh
+++ .config/fzf-preview.sh
@@ -1,31 +1,31 @@
-#! /usr/bin/zsh
+#! /usr/bin/env zsh
 
 dim=${FZF_PREVIEW_COLUMNS}x${FZF_PREVIEW_LINES}
 
 mime_type=$(file --mime-type -b "$1")
-  case $mime_type in
-    inode/directory)
-      eza -1 -T -L 1 --color=always --icons=auto "$1"
-      ;;
-    application/json | text/json)
-      jq . "$1"
-      ;;
-    text/markdown | text/x-markdown)
-      bat --style=plain --color=always "$1"
-      ;;
-    image/*)
-      chafa -s "$dim" "$1"
-      ;;
-    text/x-log | application/x-log)
-      lnav "$1"
-      ;;
-    application/pdf)
-      pdftotext "$1" - | less
-      ;;
-    text/*)
-      bat --color=always --style=numbers "$1"
-      ;;
-    *)
-      echo "Preview not available for this file type."
-      ;;
-  esac
+case $mime_type in
+  inode/directory)
+    eza -1 -T -L 1 --color=always --icons=auto "$1"
+    ;;
+  application/json | text/json)
+    jq . "$1"
+    ;;
+  text/markdown | text/x-markdown)
+    bat --style=plain --color=always "$1"
+    ;;
+  image/*)
+    chafa -s "$dim" "$1"
+    ;;
+  text/x-log | application/x-log)
+    lnav "$1"
+    ;;
+  application/pdf)
+    pdftotext "$1" - | less
+    ;;
+  text/*)
+    bat --color=always --style=numbers "$1"
+    ;;
+  *)
+    echo "Preview not available for this file type."
+    ;;
+esac
